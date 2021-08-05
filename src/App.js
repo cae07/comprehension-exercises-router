75a005277a4edbb917d6aa007698ef449c882dfc
Caê <eduardokae01@hotmail.com>
2021-08-05 15:11:13 -0300

    encapsula os Route no App com Switch

--- src/App.js
+++ src/App.js
@@ -2,19 +2,21 @@ import React, { Component } from 'react';
 import Home from './components/Home';
 import Users from './components/Users';
 import About from './components/About';
-import { BrowserRouter, Link } from 'react-router-dom';
+import { BrowserRouter, Link, Switch } from 'react-router-dom';
 import { Route } from 'react-router-dom';
 
 class App extends Component {
   render() {
     return (
       <BrowserRouter>
-        <Route exact path="/" component={Home} />
-        <Link to="/">Home</Link>
-        <Route path="/users/:id" render={(props) => <Users { ...props } greetingsMessage="Good morning" />} />
-        <Link to="/users">Users</Link>
-        <Route path="/about" component={About} />
-        <Link to="/about">About</Link>
+        <Switch>
+          <Route path="/users/:id" render={(props) => <Users { ...props } greetingsMessage="Good morning" />} />
+          <Link to="/users">Users</Link>
+          <Route path="/about" component={About} />
+          <Link to="/about">About</Link>
+          <Route exact path="/" component={Home} />
+          <Link to="/">Home</Link>
+        </Switch>
       </BrowserRouter>
     );
   }
